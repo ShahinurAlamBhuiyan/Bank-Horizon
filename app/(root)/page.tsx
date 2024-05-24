b54fc8218dae42ea487c5afb6ba0b2cbe135dd5c
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2024-05-24 21:40:07 +0600

    design headerBox component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,22 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstName: 'Shahin' }
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
-export default Home 
+export default Home
