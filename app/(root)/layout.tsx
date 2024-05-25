e48eeff99ad1eeda1ad8488f5dad28f9d84ae2e1
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2024-05-25 22:37:41 +0600

    complete left sidebar for mobile

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
+import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
 
 
 
@@ -7,11 +9,25 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const loggedIn = { firstName: 'Shahin', lastName: 'Bhuiyan'};
+  const loggedIn = { firstName: 'Shahin', lastName: 'Bhuiyan' };
   return (
     <main className="flex h-screen w-full font-inter">
       <Sidebar user={loggedIn} />
-      {children}
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image
+            src='/icons/logo.svg'
+            width={30}
+            height={30}
+            alt="logo"
+          />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
