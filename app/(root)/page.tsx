73cde8f64f75d8c60a6f4e4c3be8abd7ab5768b1
ShahinurAlamBhuiyan <shahinbhuiyan19sep@gmail.com>
2024-06-22 17:17:23 +0600

    implement [dynamic banks]

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,22 @@
 import HeaderBox from '@/components/HeaderBox'
 import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
+import { getAccount, getAccounts } from '@/lib/actions/bank.actions'
 import { getLoggedInUser } from '@/lib/actions/user.actions'
+import { SearchParamProps } from '@/types'
 import React from 'react'
 
-const Home = async() => {
+const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
   const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({ userId: loggedIn.$id })
+
+  if (!accounts) return;
+
+  const accountData = accounts?.data;
+  const appwriteItemId = (id as string) || accountData[0]?.appwriteItemId;
+
+  const account = await getAccount({ appwriteItemId });
+
   return (
     <section className='home'>
       <div className='home-content'>
@@ -13,14 +24,14 @@ const Home = async() => {
           <HeaderBox
             type="greeting"
             title="Welcome"
-            user={loggedIn?.name || 'Guest'}
+            user={loggedIn?.firstName || 'Guest'}
             subtext="Access and manage your account and transactions efficiently."
           />
 
           <TotalBalanceBox
-            accounts={[]}
-            totalBanks={1}
-            totalCurrentBalance={1250.78}
+            accounts={accountData}
+            totalBanks={accounts.totalBanks}
+            totalCurrentBalance={accounts?.totalCurrentBalance}
           />
         </header>
 
@@ -29,8 +40,8 @@ const Home = async() => {
 
       <RightSidebar
         user={loggedIn}
-        transactions={[]}
-        banks={[{ currentBalance: 143.55 }, { currentBalance: 897.44 }]}
+        transactions={accounts?.transactions}
+        banks={accountData?.slice(0, 2)}
       />
     </section>
   )
--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -2,19 +2,22 @@
 import React from 'react'
 import { Doughnut } from 'react-chartjs-2'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { DoughnutChartProps } from '@/types'
 
 ChartJS.register(ArcElement, Tooltip, Legend)
 
 const DoughnutChart = ({ accounts }: DoughnutChartProps) => {
+    const accountNames = accounts.map((a: any) => a.name);
+    const balances = accounts.map((a: any) => a.currentBalance)
     const data = {
         datasets: [
             {
                 label: 'Banks',
-                data: [1250, 2500, 3750],
+                data: balances,
                 backgroundColor: ['#0747b6', '#2265d8', '#2f91fa']
             }
         ],
-        labels: ['Bank 1', 'Bank 2', 'Bank 3']
+        labels: accountNames
     }
     return (
         <Doughnut data={data} options={{
